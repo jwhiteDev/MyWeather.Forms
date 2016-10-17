6af3c69a3263a1bf7c844871fd0baf2ea2f0ab0e
brminnick <brandon.r.minnick@gmail.com>
2016-10-17 15:41:43 -0700

    Refactored Feedback Alert on iOS

--- MyWeather.iOS/Services/HockeyappService_iOS.cs
+++ MyWeather.iOS/Services/HockeyappService_iOS.cs
@@ -17,14 +17,20 @@ namespace MyWeather.iOS
 		public async Task GiveFeedback()
 		{
 			var feedbackManager = BITHockeyManager.SharedHockeyManager.FeedbackManager;
-			var selectedButtonIndex = await ShowFeedbackAlert();
 
-			if (selectedButtonIndex == 0)
-				// Show current feedback
-				feedbackManager.ShowFeedbackListView();
-			else
-				// Send new feedback
-				feedbackManager.ShowFeedbackComposeView();
+			var alert = UIAlertController.Create("Give Feedback", "Provide Feedback to the Developers", UIAlertControllerStyle.ActionSheet);
+			alert.AddAction(UIAlertAction.Create("Cancel", UIAlertActionStyle.Cancel, null));
+			alert.AddAction(UIAlertAction.Create("Review Existing Feedback", UIAlertActionStyle.Default, (obj) => feedbackManager.ShowFeedbackListView()));
+			alert.AddAction(UIAlertAction.Create("Submit New Feedback", UIAlertActionStyle.Default, (obj) => feedbackManager.ShowFeedbackComposeView()));
+
+			var window = UIApplication.SharedApplication.KeyWindow;
+			var vc = window.RootViewController;
+			while (vc.PresentedViewController != null)
+			{
+				vc = vc.PresentedViewController;
+			}
+
+			await vc.PresentViewControllerAsync(alert, true);
 		}
 
 		public void TrackEvent(string eventName)
@@ -35,29 +41,6 @@ namespace MyWeather.iOS
 		public void TrackEvent(string eventName, Dictionary<string, string> properties, Dictionary<string, double> measurements)
 		{
 			throw new NotImplementedException();
-		}
-
-		async Task<nint> ShowFeedbackAlert()
-		{
-			var tcs = new TaskCompletionSource<nint>();
-
-			var alert = new UIAlertView
-			{
-				Title = "Give Feedback",
-			};
-			alert.AddButton("Review Existing Feedback");
-			alert.AddButton("Submit New Feedback");
-
-			alert.Clicked += (sender, buttonArgs) => tcs.SetResult(buttonArgs.ButtonIndex);
-
-			UIApplication.SharedApplication.InvokeOnMainThread(() => alert.Show());
-
-			while(alert.Visible)
-			{
-				await Task.Delay(100);
-			}
-
-			return tcs.Task.Result;
 		}
 	}
 }
